e1d8bc23d17cd1c1b572a2822002d8cc324a6650
BitSplice <salmanansari74070@gmail.com>
2025-09-02 17:52:39 +0530

    COMMIT

---
+++ Functions.java
@@ -0,0 +1,14 @@
+
+public class Functions {
+
+    public static void main(String[] args) {
+        greetUser(); // METHOD CALLING
+
+    }
+                        /*  VOID IS A RETURN TYPE 
+                            IT DOES NOT RETURN ANY VALUE
+                        */
+    public static void greetUser() {
+        System.out.println("\nGood morning\n");
+    }
+}
---
+++ Pattern.java
@@ -0,0 +1,23 @@
+
+public class Pattern {
+
+    public static void main(String[] args) {
+        printFirstpattern();
+    }
+
+    public static void printFirstpattern() {
+        System.out.println("\n*\n* *\n* * *\n* * * *\n* * * * *");
+    }
+
+    public static void printSecondpattern() {
+        System.out.println("\n\n* * * * *\n* * * *\n* * *\n* *\n*\n");
+    }
+
+    public static void printThirdpattern() {
+        System.out.println("     *\n    * *\n   * * *\n  * * * *\n * * * * *\n");
+    }
+
+    public static void printlastpattern() {
+        System.out.println("\n        *\n      * *\n    * * *\n  * * * *\n* * * * *");
+    }
+}
